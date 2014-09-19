07b009a8691fd4ec8c4824cae9c63c20b8f3dbfc
masoud gh <m.gh@linuxmail.org>
2014-09-19 04:32:27 +0330

    m

--- mrfiles/drives.php
+++ mrfiles/drives.php
@@ -8,7 +8,28 @@ function mdrmdrives()
     foreach($D as $d )
     {
         $dO = $fso->GetDrive($d);
+        $mar[] .= "Drive " . $dO->DriveLetter . " | " . $type[$dO->DriveType];
+    }
+    return $mar;
+}
+
+function mdrfile_size($size)
+{
+    $filesizename = array(" Bytes", " KB", " MB", " GB", " TB", " PB", " EB", " ZB", " YB");
+    return round($size/pow(1024, ($i = floor(log($size, 1024)))), 2) . $filesizename[$i];
+}
+function mdrdriveinformation()
+{
+    $fso = new COM('Scripting.FileSystemObject');
+    $D = $fso->Drives;
+    $type = array("Unknown","Removable","Fixed","Network","CD-ROM","RAM Disk");
+    $mar = array();
+    foreach($D as $d )
+    {
+        $dO = $fso->GetDrive($d);
         $s = "";
+        $serial = "";
+        $filesystem = "";
         if($dO->DriveType == 3)
         {
             $n = $dO->Sharename;
@@ -30,18 +51,14 @@ function mdrmdrives()
         {
             $s = "No size";
         }
-        $mar[] .= "Drive " . $dO->DriveLetter . ": - " . $type[$dO->DriveType] . " - " . $n . " - " . $s;
+        if($dO->DriveType === 2)
+        {
+            $serial =  " | serial number " .$dO->SerialNumber;
+            $filesystem = " | " .$dO->FileSystem;
+        }
+        $mar[] .= "Drive " . $dO->DriveLetter . " | " . $type[$dO->DriveType] . " | " . $n . " | " . $s . $serial . $filesystem;
+
     }
     return $mar;
-}
-
-function mdrfile_size($size)
-{
-    $filesizename = array(" Bytes", " KB", " MB", " GB", " TB", " PB", " EB", " ZB", " YB");
-    return round($size/pow(1024, ($i = floor(log($size, 1024)))), 2) . $filesizename[$i];
-}
-function mdrdriveinformation()
-{
-
 }
 ?>
